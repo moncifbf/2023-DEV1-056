00af3022f91ed800e43ea99ce71c6efb0b6166cb
Moncif Bf <moncifbounif1990@gmail.com>
2023-04-29 16:56:15 +0200

    [RED] - Return one book price test

--- DevelopmentBooks/src/main/java/com/bnp/kata/DevelopmentBooks/service/PricingService.java
+++ DevelopmentBooks/src/main/java/com/bnp/kata/DevelopmentBooks/service/PricingService.java
@@ -4,11 +4,18 @@ import com.bnp.kata.DevelopmentBooks.model.PaymentReceiptDTO;
 import com.bnp.kata.DevelopmentBooks.model.PurchaseDTO;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 @Service
 public class PricingService {
+    private static final BigDecimal BOOK_PRICE = new BigDecimal(50).setScale(2, RoundingMode.HALF_EVEN);
+
+
     public PaymentReceiptDTO getPrice(PurchaseDTO purchaseDTO) {
         PaymentReceiptDTO result = new PaymentReceiptDTO();
-
+        BigDecimal finalPrice = BOOK_PRICE.multiply(BigDecimal.valueOf(purchaseDTO.getBookQuantities().size()));
+        result.setPrice(finalPrice);
         return result;
     }
 }
