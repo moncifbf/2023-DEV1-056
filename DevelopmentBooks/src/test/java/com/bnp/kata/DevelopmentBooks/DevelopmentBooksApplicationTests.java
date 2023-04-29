8f710f6bc51ed5f357bb4aafca7517fe8c0c306b
Moncif Bf <moncifbounif1990@gmail.com>
2023-04-29 15:41:53 +0200

    init commit

---
+++ DevelopmentBooks/src/test/java/com/bnp/kata/DevelopmentBooks/DevelopmentBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnp.kata.DevelopmentBooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopmentBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
