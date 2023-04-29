064632c301cf9a0d64374a05f86936e512742d6c
Moncif Bf <moncifbounif1990@gmail.com>
2023-04-29 16:03:51 +0200

    [RED] - Add book endpoint test

---
+++ DevelopmentBooks/src/main/java/com/bnp/kata/DevelopmentBooks/service/PricingService.java
@@ -0,0 +1,14 @@
+package com.bnp.kata.DevelopmentBooks.service;
+
+import com.bnp.kata.DevelopmentBooks.model.PaymentReceiptDTO;
+import com.bnp.kata.DevelopmentBooks.model.PurchaseDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PricingService {
+    public PaymentReceiptDTO getPrice(PurchaseDTO purchaseDTO) {
+        PaymentReceiptDTO result = new PaymentReceiptDTO();
+
+        return null;
+    }
+}
---
+++ DevelopmentBooks/src/test/java/com/bnp/kata/DevelopmentBooks/endpoint/BookEndpointTest.java
@@ -0,0 +1,40 @@
+package com.bnp.kata.DevelopmentBooks.endpoint;
+
+import com.bnp.kata.DevelopmentBooks.model.PaymentReceiptDTO;
+import com.bnp.kata.DevelopmentBooks.service.PricingService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.math.BigDecimal;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BookEndpointTest.class)
+class BookEndpointTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PricingService pricingService;
+
+    @Test
+    void buyBooks_ShouldReturnPaymentReceipt() throws Exception {
+        PaymentReceiptDTO paymentReceiptDTO = new PaymentReceiptDTO();
+        paymentReceiptDTO.setPrice(new BigDecimal("50"));
+
+        when(pricingService.getPrice(any())).thenReturn((paymentReceiptDTO));
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/book/buy")
+                        .contentType(APPLICATION_JSON)
+                        .content("{\"Clean Code (Robert Martin, 2008)\":1}")
+                )
+                .andExpect(status().isOk());
+    }
+}
