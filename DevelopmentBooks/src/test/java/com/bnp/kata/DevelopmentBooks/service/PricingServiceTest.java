f8f9362bc0d2965f144f919b8075fa3da649b818
Moncif Bf <moncifbounif1990@gmail.com>
2023-04-29 17:46:08 +0200

    [REFACTOR] - Remove unwanted class, add 3,4 and 5 books test case, and put prices as constants

--- DevelopmentBooks/src/test/java/com/bnp/kata/DevelopmentBooks/service/PricingServiceTest.java
+++ DevelopmentBooks/src/test/java/com/bnp/kata/DevelopmentBooks/service/PricingServiceTest.java
@@ -13,6 +13,10 @@ import java.util.Map;
 
 class PricingServiceTest {
     public static final BigDecimal EXPECTED_ONE_BOOK_PRICE = BigDecimal.valueOf(50).setScale(1, RoundingMode.HALF_EVEN);
+    public static final BigDecimal EXPECTED_TWO_BOOKS_PRICE = BigDecimal.valueOf(100).setScale(1, RoundingMode.HALF_EVEN);
+    public static final BigDecimal EXPECTED_THREE_BOOKS_PRICE = BigDecimal.valueOf(150).setScale(1, RoundingMode.HALF_EVEN);
+    public static final BigDecimal EXPECTED_FOUR_BOOKS_PRICE = BigDecimal.valueOf(200).setScale(1, RoundingMode.HALF_EVEN);
+    public static final BigDecimal EXPECTED_FIVE_BOOKS_PRICE = BigDecimal.valueOf(250).setScale(1, RoundingMode.HALF_EVEN);
 
     private PricingService pricingService;
 
@@ -40,6 +44,39 @@ class PricingServiceTest {
         purchaseDTO.setBookQuantities(booksQuantities);
 
         PaymentReceiptDTO paymentReceiptDTO = pricingService.getPrice(purchaseDTO);
-        Assertions.assertEquals(BigDecimal.valueOf(100).setScale(1, RoundingMode.HALF_EVEN), paymentReceiptDTO.getPrice());
+        Assertions.assertEquals(EXPECTED_TWO_BOOKS_PRICE, paymentReceiptDTO.getPrice());
+    }
+
+    @Test
+    void getPrice_3() {
+        PurchaseDTO purchaseDTO = new PurchaseDTO();
+        Map<String, Integer> booksQuantities = new HashMap<>();
+        booksQuantities.put("Book1", 3);
+        purchaseDTO.setBookQuantities(booksQuantities);
+
+        PaymentReceiptDTO paymentReceiptDTO = pricingService.getPrice(purchaseDTO);
+        Assertions.assertEquals(EXPECTED_THREE_BOOKS_PRICE, paymentReceiptDTO.getPrice());
+    }
+
+    @Test
+    void getPrice_4() {
+        PurchaseDTO purchaseDTO = new PurchaseDTO();
+        Map<String, Integer> booksQuantities = new HashMap<>();
+        booksQuantities.put("Book1", 4);
+        purchaseDTO.setBookQuantities(booksQuantities);
+
+        PaymentReceiptDTO paymentReceiptDTO = pricingService.getPrice(purchaseDTO);
+        Assertions.assertEquals(EXPECTED_FOUR_BOOKS_PRICE, paymentReceiptDTO.getPrice());
+    }
+
+    @Test
+    void getPrice_5() {
+        PurchaseDTO purchaseDTO = new PurchaseDTO();
+        Map<String, Integer> booksQuantities = new HashMap<>();
+        booksQuantities.put("Book1", 5);
+        purchaseDTO.setBookQuantities(booksQuantities);
+
+        PaymentReceiptDTO paymentReceiptDTO = pricingService.getPrice(purchaseDTO);
+        Assertions.assertEquals(EXPECTED_FIVE_BOOKS_PRICE, paymentReceiptDTO.getPrice());
     }
 }
