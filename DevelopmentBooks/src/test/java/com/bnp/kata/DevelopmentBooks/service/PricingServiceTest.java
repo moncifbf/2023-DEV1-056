934f8c967345f23724ea6c0ad1711ce4c3bd4703
Moncif Bf <moncifbounif1990@gmail.com>
2023-04-29 16:25:32 +0200

    [RED] - Add price service test

---
+++ DevelopmentBooks/src/test/java/com/bnp/kata/DevelopmentBooks/service/PricingServiceTest.java
@@ -0,0 +1,32 @@
+package com.bnp.kata.DevelopmentBooks.service;
+
+import com.bnp.kata.DevelopmentBooks.model.PaymentReceiptDTO;
+import com.bnp.kata.DevelopmentBooks.model.PurchaseDTO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PricingServiceTest {
+    private PricingService pricingService;
+
+    @BeforeEach
+    void setUp() {
+        pricingService = new PricingService();
+    }
+
+    @Test
+    void getPrice_1() {
+        PurchaseDTO purchaseDTO = new PurchaseDTO();
+        Map<String, Integer> booksQuantities = new HashMap<>();
+        booksQuantities.put("Book1", 1);
+        purchaseDTO.setBookQuantities(booksQuantities);
+
+        PaymentReceiptDTO paymentReceiptDTO = pricingService.getPrice(purchaseDTO);
+        Assertions.assertEquals(50, paymentReceiptDTO.getPrice());
+    }
+}
